e05ce64746b34e66b48fe8059352b061f30bb666
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-01-10 11:08:53 +0200

    Kloonien toiminnallisuuden kokeilua

---
+++ Assets/Scripts/CloneMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 10f;
+
+    private void FixedUpdate()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+
+    }
+
+}
---
+++ Assets/Scripts/CloneSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneSpawner : MonoBehaviour
+{
+    ClonePooler clonePooler;
+
+    private void Start()
+    {
+        clonePooler = ClonePooler.Instance;
+    }
+
+    private void FixedUpdate()
+    {
+        clonePooler.SpawnFromPool("Clone", transform.position, Quaternion.identity);
+    }
+}
