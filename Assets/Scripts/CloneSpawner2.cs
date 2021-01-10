1ff25c3bf190b37c5967dd3a61884cede96f44f9
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-01-10 14:55:19 +0200

    Spawnerin fixausta toimivammaksi.

--- Assets/Scripts/CloneSpawner2.cs
+++ Assets/Scripts/CloneSpawner2.cs
@@ -7,14 +7,22 @@ public class CloneSpawner2 : MonoBehaviour
     [SerializeField] private float timeToSpawn = 5f;
     private float timeSinceSpawn;
     private ClonePooler2 clonePool;
+    [SerializeField] private float delayTime;
 
     private void Start()
     {
+        StartCoroutine(StartSpawning());
+    }
+
+    private IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(delayTime);
         clonePool = FindObjectOfType<ClonePooler2>();
     }
 
     private void Update()
     {
+        
         timeSinceSpawn += Time.deltaTime;
 
         if (timeSinceSpawn >= timeToSpawn)
