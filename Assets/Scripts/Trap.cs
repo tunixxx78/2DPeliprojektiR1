986950a7c5be13a52b14371734d97307a2b5492a
Hypocrisy95 <74293395+Hypocrisy95@users.noreply.github.com>
2021-01-22 01:07:49 +0200

    testailua

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public GameObject[] toSpawn;
+    public Transform spawnLocation;
+
+    public float minInterval = 1f, maxInterval = 2f;
+    float _nextSpawnTime = 0f;
+
+    public void Update()
+    {
+        // has enough time passed since last spawn?
+        if (Time.time >= _nextSpawnTime)
+        {
+            // delay the next spawn time by a random amount
+            _nextSpawnTime += Random.Range(minInterval, maxInterval);
+            // select and instantiate a random prefab
+            int randomIndex = Random.Range(0, toSpawn.Length);
+            Instantiate(toSpawn[randomIndex], spawnLocation.position, Quaternion.identity);
+        }
+    }
+}
