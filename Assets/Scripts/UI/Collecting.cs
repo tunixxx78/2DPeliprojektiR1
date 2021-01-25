94d9bba66c698a51e16a814a566e9a7206e39dbe
Japeee <74297488+Japeee@users.noreply.github.com>
2021-01-25 15:11:32 +0200

    Collectible system luoto
    Ja UI Scripteille oma kansio

---
+++ Assets/Scripts/UI/Collecting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collecting : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        ScoringSystem.theScore += 50;
+        Destroy(gameObject);
+    }
+
+}
