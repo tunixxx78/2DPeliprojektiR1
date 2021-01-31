dc2b086347d9236f2d47aa19ed126ef14abfec93
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-01-31 19:21:51 +0200

    Ansan kehitystä

---
+++ Assets/Scripts/Traps/ChangeSprite.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer rend;
+    [SerializeField] private Sprite IdlePlant, AtackPlant;
+
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        rend.sprite = IdlePlant;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            rend.sprite = AtackPlant;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            rend.sprite = IdlePlant;
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
