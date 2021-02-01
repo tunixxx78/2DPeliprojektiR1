b7d0a76c25096ef263fdc54b88d2310bf02ee6a7
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-02-01 12:22:15 +0200

    pyörivä apu prefab

---
+++ Assets/Scripts/Aid's/SpinningWheel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningWheel : MonoBehaviour
+{
+    private float rotZ;
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private bool clockwiseRotation;
+
+    private void Update()
+    {
+        if (clockwiseRotation == false)
+        {
+            rotZ += Time.deltaTime * rotSpeed;
+        }
+        else
+        {
+            rotZ -= Time.deltaTime * rotSpeed;
+        }
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+    }
+}
