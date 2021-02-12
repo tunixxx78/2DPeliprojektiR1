506870ea88da2be6fed641a6868b4f143edc7ceb
Hypocrisy95 <74293395+Hypocrisy95@users.noreply.github.com>
2021-02-12 19:28:02 +0200

    muutokset

---
+++ Assets/Scripts/Weapon/BulletPrefabV1/Arrow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x * Mathf.Rad2Deg);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
