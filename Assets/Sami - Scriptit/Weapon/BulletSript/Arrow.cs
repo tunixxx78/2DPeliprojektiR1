b2c284a07bcd191547ecb332e1766703c9fbb714
Hypocrisy95 <74293395+Hypocrisy95@users.noreply.github.com>
2021-02-12 22:55:16 +0200

    Luotu alustava ampuminen

--- Assets/Sami - Scriptit/Weapon/BulletSript/Arrow.cs
+++ Assets/Sami - Scriptit/Weapon/BulletSript/Arrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     Rigidbody2D rb;
+    bool hasHit;
 
     private void Start()
     {
@@ -13,7 +14,16 @@ public class Arrow : MonoBehaviour
 
     void Update()
     {
-        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x * Mathf.Rad2Deg);
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (hasHit == false)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x * Mathf.Rad2Deg);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasHit = true;
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
     }
 }
--- Assets/Sami - Scriptit/Weapon/Crossbow.cs
+++ Assets/Sami - Scriptit/Weapon/Crossbow.cs
@@ -8,23 +8,58 @@ public class Crossbow : MonoBehaviour
     public float launchForce;
     public Transform shotPoint;
 
+    //  ↓ Path point
+    public GameObject point;
+    GameObject[] points;
+    public int numberOfPoints;
+    public float spaceBeetweenPoints;
+    // ↓ Line 17 poista jos Path point ei käytössä
+    Vector2 direction;
+
+
+
+    private void Start()
+    {
+        points = new GameObject[numberOfPoints];
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i] = Instantiate(point, shotPoint.position, Quaternion.identity);
+        }
+    }
+
+    //  ↑
+
     void Update()
     {
         Vector2 bowPosition = transform.position;
-
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePosition - bowPosition;
+        // ↓ Line 37 lisää vector2 jos Path Point ei käytössä.
+        direction = mousePosition - bowPosition;
         transform.right = direction;
 
         if (Input.GetMouseButtonDown(0))
         {
             Shoot();
         }
-
-        void Shoot()
+        //  ↓ Path Point code
+        for (int i = 0; i < numberOfPoints; i++)
         {
-            GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
-            newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
+            points[i].transform.position = PointPosition(i * spaceBeetweenPoints);
         }
+        //  ↑
     }
+    void Shoot()
+    {
+        GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
+        newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
+    }
+    
+    //  ↓ Path Point code
+
+    Vector2 PointPosition(float t) {
+        Vector2 position = (Vector2)shotPoint.position + (direction.normalized * launchForce * t) + 0.5f * Physics2D.gravity * (t * t);
+        return position;
+    }
+    
+    //  ↑
 }
