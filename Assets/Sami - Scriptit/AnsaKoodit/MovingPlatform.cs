3826a226e9e0a289494ad74c43649fea8845d6da
Hypocrisy95 <74293395+Hypocrisy95@users.noreply.github.com>
2021-02-13 14:10:34 +0200

    MovingPlatformV1

---
+++ Assets/Sami - Scriptit/AnsaKoodit/MovingPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    float dirX, moveSpeed = 3f;
+    bool moveRight = true;
+
+    void Update()
+    {
+        if (transform.position.x > 4f)
+            moveRight = false;
+        if (transform.position.x < -4f)
+            moveRight = true;
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else;
+        transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+
+    }
+}
