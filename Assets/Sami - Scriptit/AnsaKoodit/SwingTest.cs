ab5c3667774a979ab849ca3c6ca47cc74aebcb49
Hypocrisy95 <74293395+Hypocrisy95@users.noreply.github.com>
2021-02-13 04:53:17 +0200

    trap muutoksia

---
+++ Assets/Sami - Scriptit/AnsaKoodit/SwingTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingTest : MonoBehaviour
+{
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    public float direction = 1;
+    private Quaternion startPos;
+    void Start()
+    {
+        startPos = transform.rotation;
+    }
+    void Update()
+    {
+        Quaternion a = startPos;
+        a.z += direction * (delta * Mathf.Sin(Time.time * speed));
+        transform.rotation = a;
+    }
+}
