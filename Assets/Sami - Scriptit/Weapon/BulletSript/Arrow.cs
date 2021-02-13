82d3dec0411012f941ac9671240db725b3d403ab
Hypocrisy95 <74293395+Hypocrisy95@users.noreply.github.com>
2021-02-13 03:29:16 +0200

    Viimeistelyä
    Ampuma koodi korjattu.
    Layereitä säädetty ja koodia hiukan muokattu.

--- Assets/Sami - Scriptit/Weapon/BulletSript/Arrow.cs
+++ Assets/Sami - Scriptit/Weapon/BulletSript/Arrow.cs
@@ -6,7 +6,6 @@ public class Arrow : MonoBehaviour
 {
     private Rigidbody2D rb;
     private bool hasHit;
-   
     
 
     private void Start()
@@ -18,8 +17,22 @@ public class Arrow : MonoBehaviour
     {
         if (hasHit == false)
         {
-            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x * Mathf.Rad2Deg);
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+           // /* - Parempi?
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+           // */
+
+            // original float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x * Mathf.Rad2Deg);
+            // original transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+           /*
+            Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+            mousePos.x = mousePos.x - objectPos.x;
+            mousePos.y = mousePos.y - objectPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+           */
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
