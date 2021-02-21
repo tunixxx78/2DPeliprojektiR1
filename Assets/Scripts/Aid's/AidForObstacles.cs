613a5a947f41047eb92d1bffb3aea798cf433763
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-02-21 16:58:15 +0200

    Toisen levelin suunnittelua ja toteutusta

--- Assets/Scripts/Aid's/AidForObstacles.cs
+++ Assets/Scripts/Aid's/AidForObstacles.cs
@@ -6,15 +6,14 @@ public class AidForObstacles : MonoBehaviour
 {
     [SerializeField] private GameObject aid, aid1, aid2;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             Destroy(aid);
             Destroy(aid1);
             Destroy(aid2);
         }
-
         else
         {
             return;
