f0f3b37eaa893b2759668fc5e35e91af8c1263e9
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-02-21 14:01:24 +0200

    Uuden levelin suunnittelua

---
+++ Assets/Scripts/Aid's/AidForObstacles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AidForObstacles : MonoBehaviour
+{
+    [SerializeField] private GameObject aid, aid1, aid2;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Destroy(aid);
+            Destroy(aid1);
+            Destroy(aid2);
+        }
+
+        else
+        {
+            return;
+        }
+    }
+}
