90bccb3377b37ba54aef2d9cab2e1e232c6432a5
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-03-01 16:56:38 +0200

    Kloonihahmoon
    Pieniä päivityksiä kloonin toiminnallisuuteen
    Kloonin matkan indicatorin aloitus....

---
+++ Assets/Scripts/ClonePathIndicator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonePathIndicator : MonoBehaviour
+{
+    [SerializeField] private GameObject indicator;
+
+    public void PathIndicator()
+    {
+        indicator.SetActive(false);
+    }
+}
