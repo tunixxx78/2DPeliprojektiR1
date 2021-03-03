14d3341d958fd9b57fd0e7d473b36956be5da27c
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-03-03 17:06:20 +0200

    päivitystä

---
+++ Assets/Scripts/FadeMusic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeMusic : MonoBehaviour
+{
+   
+
+    private void Start()
+    {
+       FMODUnity.RuntimeManager.CreateInstance("event:/Music");
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("EndZone"))
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("endFade", 1f);
+            Debug.Log("OSUMA");
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("EndZone"))
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("endFade", 0f);
+            Debug.Log("POISTUI");
+        }
+       
+    }
+}
