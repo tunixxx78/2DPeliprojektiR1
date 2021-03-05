249a55c4f391c645d447ed764dcd389dba8dc359
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-03-05 09:58:17 +0200

    fixii
    Kloonisysteemin muutosta
    levelien fixausta -> kaikissa käytössä nyt URP
    Pistelaskun lisäys maaliviivaan

---
+++ Assets/Scripts/Aid's/ButtonToReleaseClones.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonToReleaseClones : MonoBehaviour
+{
+    [SerializeField] private GameObject clone1, clone2, clone3, clone4, cloneLast, clonepath;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            clone1.SetActive(true);
+        }
+        if (collision.CompareTag("Player"))
+        {
+            clone2.SetActive(true);
+        }
+        if (collision.CompareTag("Player"))
+        {
+            clone3.SetActive(true);
+        }
+        if (collision.CompareTag("Player"))
+        {
+            clone4.SetActive(true);
+        }
+        if (collision.CompareTag("Player"))
+        {
+            cloneLast.SetActive(true);
+        }
+        if (collision.CompareTag("Player"))
+        {
+            clonepath.SetActive(true);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
