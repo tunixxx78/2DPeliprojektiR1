eda92d1adce46bf9d1d08e88774bc6cc38f6677c
Japeee <74297488+Japeee@users.noreply.github.com>
2021-03-07 23:15:14 +0200

    Double Jump/Highscoren kikkailua

--- Assets/Scripts/Movement/PlayerCharacterController.cs
+++ Assets/Scripts/Movement/PlayerCharacterController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCharacterController : MonoBehaviour
 {
-    
+
     Rigidbody2D rb;
     public float speed;
     public float jumpForce;
@@ -14,7 +14,13 @@ public class PlayerCharacterController : MonoBehaviour
     [SerializeField] private float sizeMultiplier = 1f; //Turo lisäsi tämän
     [SerializeField] private GameObject player, reStart; //Turo lisäsi
     [SerializeField] private Animator animator;
+
+    public int extraJumps = 1;
+    int jumpCount = 0;
+    float jumpCooldown;
     
+    
+
 
     public float rememberGroundedFor;
     float lastTimeGrounded;
@@ -35,10 +41,10 @@ public class PlayerCharacterController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
     }
 
-    
+
     void Update()
     {
         Move();
@@ -58,10 +64,11 @@ public class PlayerCharacterController : MonoBehaviour
             FMODUnity.RuntimeManager.PlayOneShot("event:/Shoot", GetComponent<Transform>().position);
             animator.SetTrigger("Attack");
         }
-        
+
 
     }
 
+    
     void Move()
     {
         float x = Input.GetAxisRaw("Horizontal");
@@ -90,9 +97,10 @@ public class PlayerCharacterController : MonoBehaviour
             animator.SetBool("InAir", true);
         }
         
-        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor || jumpCount < extraJumps))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount++;
             animator.SetBool("InAir", true);
             FMODUnity.RuntimeManager.PlayOneShot("event:/Jump", GetComponent<Transform>().position);
         }
@@ -101,6 +109,7 @@ public class PlayerCharacterController : MonoBehaviour
             //animator.SetBool("InAir", false);
             
         }
+        CheckIfGrounded();
     }
 
     void CheckIfGrounded()
@@ -109,16 +118,21 @@ public class PlayerCharacterController : MonoBehaviour
         if (colliders != null)
         {
             isGrounded = true;
+            jumpCount = 0;
+            jumpCooldown = Time.time + 0.2f;
             animator.SetBool("InAir", false);
-            
-            
+
+        }
+        else if (Time.time < jumpCooldown)
+        {
+            isGrounded = true;
         }
         else
         {
-            if (isGrounded)
+           /* if (isGrounded)
             {
                 lastTimeGrounded = Time.time;
-            }
+            }*/
             isGrounded = false;
             animator.SetBool("runAnim", false);
         }
