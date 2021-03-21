2d7e947e2a10e803936302be06e5bafbaff01b8c
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-03-21 15:07:13 +0200

    Fixailua
    Tutorial ookoo
    Ykkös level ookoo tai ainakin toimiva

---
+++ Assets/Scripts/Aid's/CloneCage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneCage : MonoBehaviour
+{
+    [SerializeField] private GameObject camera1;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject clone1, clone2, clone3, clone4, cloneLast, home;
+
+    public void ToCage()
+    {
+        camera1.SetActive(true);
+        animator.SetTrigger("open");
+    }
+
+    public void BackToPlayer()
+    {
+        camera1.SetActive(false);
+        clone1.SetActive(true);
+        clone2.SetActive(true);
+        clone3.SetActive(true);
+        clone4.SetActive(true);
+        cloneLast.SetActive(true);
+        home.SetActive(true);
+
+    }
+}
