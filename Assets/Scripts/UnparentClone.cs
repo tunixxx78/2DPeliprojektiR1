3f16d3a18b4b6beba83ba9cecdd2d0df5f4c702a
Tatu Uusitalo <tatu.uusitalo@gmail.com>
2021-03-23 17:35:18 +0200

    Unparent Klooni script

---
+++ Assets/Scripts/UnparentClone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnparentClone : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] clonesToUnparent;
+   public void Unparent()
+    {
+        for (int i = 0; i < clonesToUnparent.Length; i++)
+        {
+            clonesToUnparent[i].transform.parent = null;
+        }
+    }
+}
