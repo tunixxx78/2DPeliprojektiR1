83b28ba5fbf58bd1cfbb9c158aad63917b4ff086
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-03-27 17:18:29 +0200

    Updatee
    Pientä fixii
    Highscoresystem

---
+++ Assets/Scripts/Turon Testit/HighScoreScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HighScoreScreen : MonoBehaviour
+{
+    public void StartAgain()
+    {
+        SceneManager.LoadScene("Turo");
+        ScoringSystem.theScore = (int) 0f;
+    }
+
+    public void LeaveGame()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ResetScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        FindObjectOfType<HighScoreSystem>().ZeroScore();
+    }
+}
---
+++ Assets/Scripts/Turon Testit/HighScoreSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreSystem : MonoBehaviour
+{
+    public Text highScore, yourScore;
+    [SerializeField] private GameObject currentScore;
+
+    private void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        //currentScore.GetComponent<Text>().text = "" + yourScore;
+        yourScore.text = ScoringSystem.theScore.ToString();
+    }
+
+    public void ZeroScore()
+    {
+        highScore.text = "0";
+    }
+}
